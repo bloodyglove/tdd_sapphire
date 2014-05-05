0f56670423006240f67c8fcb4f5bd8323a9262f9
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2014-05-05 19:08:02 +0700

    use mock for test

--- CaptchaService.php
+++ CaptchaService.php
@@ -2,9 +2,18 @@
 require_once 'Captcha.php';
 require_once 'Randomizer.php';
 class CaptchaService {
+  private $random;
+
+  function setRandom($random) {
+    $this->random = $random;
+  }
+
+  function __construct() {
+    $this->random = new Randomizer();
+  }
 
   function randomCaptcha() {
-    $random = new Randomizer();
+    $random = $this->random;
     return new Captcha($random->operand(), $random->operator(), $random->operand());
   }
 }
--- CaptchaServiceSpec.php
+++ CaptchaServiceSpec.php
@@ -5,11 +5,25 @@ class CaptchaServiceSpec extends PHPUnit_Framework_TestCase {
     $captchaService = new CaptchaService();
     $this->assertTrue($captchaService->randomCaptcha() instanceof Captcha);
   }
-
+/*
   function testRandomOperandShouldReturnInRange1to9() {
     $captchaService = new CaptchaService();
     $captcha = $captchaService->randomCaptcha();
     $this->assertEquals(4, $captcha->result());
   }
+ */
 
+  function testRandomOperandShouldReturnInRange1to9() {
+    $captchaService = new CaptchaService();
+    $mockRandom = $this->getMock('Randomizer');
+    $mockRandom->expects($this->any())
+               ->method('operand')
+               ->will($this->returnValue(2));
+    $mockRandom->expects($this->once())
+               ->method('operator')
+               ->will($this->returnValue(1));
+    $captchaService->setRandom($mockRandom);
+    $captcha = $captchaService->randomCaptcha();
+    $this->assertEquals(4, $captcha->result());
+  }
 }
